69839b472c5aa5b35df60a9a1b03c4ccbd351677
Andre Santos <andrefelipe_santos@outlook.com>
2025-03-31 16:38:44 -0300

    Tipos de péradores

---
+++ operacao_aritimetica.py
@@ -0,0 +1,13 @@
+num1 = 20
+num2 = 10
+
+print(num1  - num2)
+print(num1 + num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 * num2)
+print(num1 % num2)
+print(num1 ** num2)
+
+x = (10 + 5) * 4
+print(x)
