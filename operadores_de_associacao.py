8023dd24243ccdae10c7f20a327ecde9083bc77f
Andre Santos <andrefelipe_santos@outlook.com>
2025-03-31 16:47:05 -0300

    Excluindo arquivos duplicados

--- operadores_de_associacao.py
+++
@@ -1,9 +0,0 @@
-curso = "Curso de Python"
-frutas = ["laranja", "uva", "limão"]
-saques = [1500, 100]
-
-print("Python" in curso)
-print("maçã" in frutas)
-print("maçã" not in frutas)
-print(100 in saques)
-print(200 in saques)
--- operadores_logicos.py
+++
@@ -1,6 +0,0 @@
-saldo = 500
-saque = 250
-
-print(saldo < saque and saque == 200)
-print(saldo < saque and saque != 200)
-print(saldo < saque or saque != 200)
